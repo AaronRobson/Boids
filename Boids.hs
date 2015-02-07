11b994dbdb1bfbc402901f51142872c65c9113ae
Aaron Robson <ARobsonSE@hotmail.co.uk>
2015-02-07 04:25:56 +0000

    Object data types and applyAcceleration functions.
    applyAcceleration requires testing.
    I did experiment with a typeclass around the object typea as much of the
    code could be shared, it got very confusing.

--- Boids.hs
+++ Boids.hs
@@ -86,6 +86,45 @@ type Acceleration1d = Vector1d
 type Acceleration2d = Vector2d
 type Acceleration3d = Vector3d
 
+data Object1d = Object1d
+  { location1d :: Location1d
+  , velocity1d :: Vector1d
+  } deriving (Show, Eq)
+
+data Object2d = Object2d
+  { location2d :: Location2d
+  , velocity2d :: Vector2d
+  } deriving (Show, Eq)
+
+data Object3d = Object3d
+  { location3d :: Location3d
+  , velocity3d :: Vector3d
+  } deriving (Show, Eq)
+
+applyAcceleration1d :: Object1d -> Acceleration1d -> Object1d
+applyAcceleration1d o a = Object1d {location1d = l', velocity1d = v'}
+    where
+      v = velocity1d o
+      v' = v `addV` a
+      l = location1d o
+      l' = l `addV` v'
+
+applyAcceleration2d :: Object2d -> Acceleration2d -> Object2d
+applyAcceleration2d o a = Object2d {location2d = l', velocity2d = v'}
+    where
+      v = velocity2d o
+      v' = v `addV` a
+      l = location2d o
+      l' = l `addV` v'
+
+applyAcceleration3d :: Object3d -> Acceleration3d -> Object3d
+applyAcceleration3d o a = Object3d {location3d = l', velocity3d = v'}
+    where
+      v = velocity3d o
+      v' = v `addV` a
+      l = location3d o
+      l' = l `addV` v'
+
 {-
 --Default Number of dimensions.
 --type Vector = Vector2d
